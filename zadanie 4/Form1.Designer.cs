4290a81f9cee7a83fe12c4be3031c4e0c7485e96
Eliasz Banaszak <eliasz.banaszak06@gmail.com>
2026-04-07 23:19:24 +0200

    Gotowe - gałąź A

--- zadanie 4/Form1.Designer.cs
+++ zadanie 4/Form1.Designer.cs
@@ -56,23 +56,6 @@
             this.btnRotate270.Text = "Rotate 270°";
             this.btnRotate270.Click += new System.EventHandler(this.btnRotate270_Click);
 
-            // btnInvert
-            this.btnInvert.Location = new System.Drawing.Point(630, 180);
-            this.btnInvert.Size = new System.Drawing.Size(140, 30);
-            this.btnInvert.Text = "Invert Colors";
-            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
-
-            // btnUpsideDown
-            this.btnUpsideDown.Location = new System.Drawing.Point(630, 220);
-            this.btnUpsideDown.Size = new System.Drawing.Size(140, 30);
-            this.btnUpsideDown.Text = "Upside Down";
-            this.btnUpsideDown.Click += new System.EventHandler(this.btnUpsideDown_Click);
-
-            // btnOnlyGreen
-            this.btnOnlyGreen.Location = new System.Drawing.Point(630, 260);
-            this.btnOnlyGreen.Size = new System.Drawing.Size(140, 30);
-            this.btnOnlyGreen.Text = "Only Green";
-            this.btnOnlyGreen.Click += new System.EventHandler(this.btnOnlyGreen_Click);
 
             // Form1
             this.ClientSize = new System.Drawing.Size(800, 430);
