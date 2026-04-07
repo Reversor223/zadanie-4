345b844e39e78ff8902840ff43461845de7ef2e6
Eliasz Banaszak <eliasz.banaszak06@gmail.com>
2026-04-07 22:42:32 +0200

    Dodaj pliki projektów.

---
+++ zadanie 4/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace zadanie_4
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnRotate90 = new System.Windows.Forms.Button();
+            this.btnRotate180 = new System.Windows.Forms.Button();
+            this.btnRotate270 = new System.Windows.Forms.Button();
+            this.btnInvert = new System.Windows.Forms.Button();
+            this.btnUpsideDown = new System.Windows.Forms.Button();
+            this.btnOnlyGreen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+
+            // pictureBox1
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            // btnLoad
+            this.btnLoad.Location = new System.Drawing.Point(630, 20);
+            this.btnLoad.Size = new System.Drawing.Size(140, 30);
+            this.btnLoad.Text = "Load Image";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+
+            // btnRotate90
+            this.btnRotate90.Location = new System.Drawing.Point(630, 60);
+            this.btnRotate90.Size = new System.Drawing.Size(140, 30);
+            this.btnRotate90.Text = "Rotate 90°";
+            this.btnRotate90.Click += new System.EventHandler(this.btnRotate90_Click);
+
+            // btnRotate180
+            this.btnRotate180.Location = new System.Drawing.Point(630, 100);
+            this.btnRotate180.Size = new System.Drawing.Size(140, 30);
+            this.btnRotate180.Text = "Rotate 180°";
+            this.btnRotate180.Click += new System.EventHandler(this.btnRotate180_Click);
+
+            // btnRotate270
+            this.btnRotate270.Location = new System.Drawing.Point(630, 140);
+            this.btnRotate270.Size = new System.Drawing.Size(140, 30);
+            this.btnRotate270.Text = "Rotate 270°";
+            this.btnRotate270.Click += new System.EventHandler(this.btnRotate270_Click);
+
+            // btnInvert
+            this.btnInvert.Location = new System.Drawing.Point(630, 180);
+            this.btnInvert.Size = new System.Drawing.Size(140, 30);
+            this.btnInvert.Text = "Invert Colors";
+            this.btnInvert.Click += new System.EventHandler(this.btnInvert_Click);
+
+            // btnUpsideDown
+            this.btnUpsideDown.Location = new System.Drawing.Point(630, 220);
+            this.btnUpsideDown.Size = new System.Drawing.Size(140, 30);
+            this.btnUpsideDown.Text = "Upside Down";
+            this.btnUpsideDown.Click += new System.EventHandler(this.btnUpsideDown_Click);
+
+            // btnOnlyGreen
+            this.btnOnlyGreen.Location = new System.Drawing.Point(630, 260);
+            this.btnOnlyGreen.Size = new System.Drawing.Size(140, 30);
+            this.btnOnlyGreen.Text = "Only Green";
+            this.btnOnlyGreen.Click += new System.EventHandler(this.btnOnlyGreen_Click);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnRotate90);
+            this.Controls.Add(this.btnRotate180);
+            this.Controls.Add(this.btnRotate270);
+            this.Controls.Add(this.btnInvert);
+            this.Controls.Add(this.btnUpsideDown);
+            this.Controls.Add(this.btnOnlyGreen);
+            this.Text = "Image Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnRotate90;
+        private System.Windows.Forms.Button btnRotate180;
+        private System.Windows.Forms.Button btnRotate270;
+        private System.Windows.Forms.Button btnInvert;
+        private System.Windows.Forms.Button btnUpsideDown;
+        private System.Windows.Forms.Button btnOnlyGreen;
+    }
+}
